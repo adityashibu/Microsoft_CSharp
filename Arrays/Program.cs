99893b2175b8f47159c925fe50515d411ccecb6a
Aditya Shibu <as2397@hw.ac.uk>
2024-06-10 22:11:37 +0400

    Modified arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,8 +1,4 @@
-﻿string[] fraudulentOrderIDs = new string[3];
-
-fraudulentOrderIDs[0] = "A123";
-fraudulentOrderIDs[1] = "B456";
-fraudulentOrderIDs[2] = "C789";
+﻿string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
 
 for (int i = 0; i < 3; i++)
 {
