08bdf6957ae909b2b2e34680b3fbd92d0499a08f
Aditya Shibu <as2397@hw.ac.uk>
2024-06-10 22:28:08 +0400

    Added and updated arrays to include foreach

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -5,4 +5,19 @@ for (int i = 0; i < 3; i++)
     Console.WriteLine(fraudulentOrderIDs[i]);
 }
 
-Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent transactions to process.");
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent transactions to process. \n");
+
+string[] names = { "John", "Paul", "George", "Ringo" };
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+foreach (int items in inventory)
+{
+    Console.WriteLine($"Inventory count: {items}");
+    sum += items;
+    Console.WriteLine($"Sum: {sum} \n");
+}
