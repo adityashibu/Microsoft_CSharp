a3b9d9f147d8f64102e2c4fa3e78b075eea5c76f
Aditya Shibu <as2397@hw.ac.uk>
2024-06-10 12:23:21 +0400

    Added code for random dice

--- DiceRoll/Program.cs
+++ DiceRoll/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random Dice = new Random();
+
+int roll1 = Dice.Next(1, 7);
+int roll2 = Dice.Next(1, 7);
+int roll3 = Dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
