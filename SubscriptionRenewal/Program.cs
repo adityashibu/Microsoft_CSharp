e1cf28d3e6d8b8e367491d60404521a0194b179b
Aditya Shibu <as2397@hw.ac.uk>
2024-06-10 17:10:06 +0400

    Added code for subscription code

--- SubscriptionRenewal/Program.cs
+++ SubscriptionRenewal/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration <= 10 && daysUntilExpiration > 5)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else if (daysUntilExpiration <= 5 && daysUntilExpiration > 1)
+{
+    discountPercentage = 10;
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days. \nRenew now and save {discountPercentage}%");
+}
+else if (daysUntilExpiration == 1)
+{
+    discountPercentage = 20;
+    Console.WriteLine($"Your subscription expires within a day! \nRenew now and save {discountPercentage}%");
+}
+else
+{
+    Console.WriteLine("Your subscription has expired.");
+}
