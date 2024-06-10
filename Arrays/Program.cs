ee5273076777a4feb5b93d6cfd966462046f6083
Aditya Shibu <as2397@hw.ac.uk>
2024-06-10 17:41:14 +0400

    Modified code for arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine(fraudulentOrderIDs[i]);
+}
