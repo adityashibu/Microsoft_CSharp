f68dd0c92b06bd0dab8dea05510e358c06592eac
Aditya Shibu <as2397@hw.ac.uk>
2024-06-10 11:58:03 +0400

    Added gitignore and code for greaterNumber using .NET libraries

--- GreaterNumber/Program.cs
+++ GreaterNumber/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int firstNumber = 600;
+int secondNumber = 300;
+
+int largerNumber;
+largerNumber = System.Math.Max(firstNumber, secondNumber);
+Console.WriteLine($"Larger Number is {largerNumber}");
