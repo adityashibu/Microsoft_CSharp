bf782f4a4b6bccefd88aa90a1a7dde25a195384a
Aditya Shibu <as2397@hw.ac.uk>
2024-06-11 14:12:02 +0400

    Completed variable scope and added files for SwitchStatemenrt

--- VariableScope/Program.cs
+++ VariableScope/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+
+foreach (int number in numbers)
+{
+    total += number;
+
+    if (number == 42)
+    {
+        bool found = true;
+        Console.WriteLine("Set contains 42");
+    }
+}
+
+Console.WriteLine($"Total: {total}");
