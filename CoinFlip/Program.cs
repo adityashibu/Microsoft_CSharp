44cad35fc36a64d4a1d7efaa6ec6eceec3daf7bc
Aditya Shibu <as2397@hw.ac.uk>
2024-06-11 13:16:25 +0400

    Completed code challenge for Random Coin flip

--- CoinFlip/Program.cs
+++ CoinFlip/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Initialize a random number generator for numbers from 0 to 1
+Random coin = new Random();
+int flip = coin.Next(0, 2);
+
+// Decide if it's heads or tails, if it's 0 then heads else tails
+Console.WriteLine($"{(flip == 0 ? "Heads" : "Tails")}");
