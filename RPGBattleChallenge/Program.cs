9188d6dcb307f9363d5c5c9fea97cc48bfdfc784
Aditya Shibu <as2397@hw.ac.uk>
2024-06-12 00:53:19 +0400

    Completed code challenge

--- RPGBattleChallenge/Program.cs
+++ RPGBattleChallenge/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random attack = new Random();
+
+int monsterHealth = 10;
+int playerHealth = 10;
+
+while (monsterHealth > 0 && playerHealth > 0)
+{
+    int playerAttack = attack.Next(1, 10);
+    int monsterAttack = attack.Next(1, 10);
+
+    monsterHealth -= playerAttack;
+    playerHealth -= monsterAttack;
+
+    Console.WriteLine($"Monster was damaged and lost {playerAttack} health and now has {monsterHealth} health.");
+    if (monsterHealth <= 0)
+    {
+        Console.WriteLine("Hero wins!");
+        break;
+    }
+
+    Console.WriteLine($"Hero was damaged and lost {monsterAttack} health and now has {playerHealth} health.");
+    if (playerHealth <= 0)
+    {
+        Console.WriteLine("Monster wins!");
+        break;
+    }
+}
