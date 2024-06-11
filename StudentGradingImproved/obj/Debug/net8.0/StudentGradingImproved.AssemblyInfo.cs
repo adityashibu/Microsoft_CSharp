28a06fa39f8eaba103feea505a7321eaa70e48cc
Aditya Shibu <as2397@hw.ac.uk>
2024-06-11 12:59:13 +0400

    Added files for coinFlip

--- StudentGradingImproved/obj/Debug/net8.0/StudentGradingImproved.AssemblyInfo.cs
+++ StudentGradingImproved/obj/Debug/net8.0/StudentGradingImproved.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentGradingImproved")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5182cac50e11b4d292ccae8c5cd917076afeab57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bb66059464b1490bb91c901516fc4b9be97479f")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentGradingImproved")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentGradingImproved")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
