eba3f8d054e9cdab136e188aff53d9a1fa7010f7
Aditya Shibu <as2397@hw.ac.uk>
2024-06-12 17:21:23 +0400

    Completed Sort and Errors code challenge

--- SortAndErrors/Program.cs
+++ SortAndErrors/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] orders = orderStream.Split(',');
+int ordersCount = orders.Length;
+
+Array.Sort(orders);
+
+for (int i = 0; i < ordersCount; i++)
+{
+    int wordLength = orders[i].Length;
+    if (wordLength > 4 || wordLength < 4)
+    {
+        Console.WriteLine($"{orders[i]}\t- Error");
+    }
+    else
+    {
+        Console.WriteLine(orders[i]);
+    }
+}
