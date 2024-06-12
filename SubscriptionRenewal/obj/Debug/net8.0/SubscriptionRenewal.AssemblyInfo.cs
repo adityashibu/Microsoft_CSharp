ec925d3895179a45a1968df4b0fab2c37811829c
Aditya Shibu <as2397@hw.ac.uk>
2024-06-12 16:04:56 +0400

    Dont know what changed here again

--- SubscriptionRenewal/obj/Debug/net8.0/SubscriptionRenewal.AssemblyInfo.cs
+++ SubscriptionRenewal/obj/Debug/net8.0/SubscriptionRenewal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SubscriptionRenewal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+122b38c145538a72b777a906ccae2237b67e72fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0a02d2282ace06126558b605666374597be7ea1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SubscriptionRenewal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SubscriptionRenewal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
