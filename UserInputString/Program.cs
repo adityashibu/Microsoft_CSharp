275aebf26bbe715e3f08725838942cc9009e2872
Aditya Shibu <as2397@hw.ac.uk>
2024-06-12 11:11:56 +0400

    Completed string parsing

---
+++ UserInputString/Program.cs
@@ -0,0 +1,15 @@
+﻿string? readResult;
+
+do
+{
+    Console.Write("Enter your role name (Administrator, Manager, or User): ");
+    readResult = Console.ReadLine()?.Trim().ToLower();
+
+    if (readResult == null || (!readResult.Contains("administrator") && !readResult.Contains("manager") && !readResult.Contains("user")))
+    {
+        Console.WriteLine($"The role name '{readResult}' is not valid.");
+    }
+}
+while (readResult == null || !readResult.Contains("administrator") && !readResult.Contains("manager") && !readResult.Contains("user"));
+
+Console.WriteLine($"Your input value {readResult} has been accepted");
