66b5bbbe63f4b86d3511eb02e434704062576649
Aditya Shibu <as2397@hw.ac.uk>
2024-06-12 16:55:30 +0400

    Updated all builds

--- GreaterNumber/obj/Debug/net8.0/GreaterNumber.AssemblyInfo.cs
+++ GreaterNumber/obj/Debug/net8.0/GreaterNumber.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GreaterNumber")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0a02d2282ace06126558b605666374597be7ea1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f3dd4f41fc38292137b5c385636ff37a8561cb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GreaterNumber")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GreaterNumber")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- StringReverse/Program.cs
+++ StringReverse/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+
+string[] words = pangram.Split(' ');
+string reversedPangram = "";
+
+for (int i = words.Length - 1; i >= 0; i--)
+{
+    words[i] = words[i].Reverse();
+    reversedPangram += words[i] + " ";
+}
