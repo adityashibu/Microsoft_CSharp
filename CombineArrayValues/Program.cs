c0a02d2282ace06126558b605666374597be7ea1
Aditya Shibu <as2397@hw.ac.uk>
2024-06-12 16:02:22 +0400

    Completed code challenge to combine array values

--- CombineArrayValues/Program.cs
+++ CombineArrayValues/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+int valuesCount = values.Length;
+
+decimal total = 0;
+string message = "";
+
+for (int i = 0; i < valuesCount; i++)
+{
+    decimal value;
+    if (decimal.TryParse(values[i], out value))
+    {
+        total += value;
+    }
+    else
+    {
+        message += values[i];
+    }
+}
+
+Console.WriteLine("Message: " + message);
+Console.WriteLine("Total: " + total);
