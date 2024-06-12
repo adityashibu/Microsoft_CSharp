8a5932e09407681fc1436a6b9a47fdaf9afd2612
Aditya Shibu <as2397@hw.ac.uk>
2024-06-12 10:57:35 +0400

    Completed readResult

--- UserInput/Program.cs
+++ UserInput/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int? readResult = 0; // Initialize to null
+
+do
+{
+    Console.Write("Enter a number between 5 and 10: ");
+    string input = Console.ReadLine();
+
+    if (int.TryParse(input, out int number))
+    {
+        readResult = number;
+    }
+    else
+    {
+        Console.WriteLine("Invalid input. Please enter a number.");
+    }
+} while (readResult == null || readResult < 5 || readResult > 10);
+
+Console.WriteLine($"You entered: {readResult}");
