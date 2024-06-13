ab1deb74fee9966b28917da748ee49f1120f6048
Aditya Shibu <as2397@hw.ac.uk>
2024-06-13 22:33:49 +0400

    Completed string interpolation

--- StringInterpolation/Program.cs
+++ StringInterpolation/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+// Your logic here
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+// Your logic here
+comparisonMessage = $"Dear {customerName},\nAs a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n\nCurrently you own {currentShares:N} shares at a return of {currentReturn:P2}.\n\nOur new product, {newProduct} offers a return of {newReturn:P2}. Given your current volume, your potential profit would be {newProfit:C}.\n\nHere is a quick comparison:\n\nMagic Yield\t\t{currentReturn:P2}\t{currentProfit:C}\n{newProduct}\t\t{newReturn:P2}\t{newProfit:C}";
+
+Console.WriteLine(comparisonMessage);
