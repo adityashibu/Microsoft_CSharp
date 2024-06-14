c92b595dba97ac2032a47ace0acdf77bc548bb5f
Aditya Shibu <as2397@hw.ac.uk>
2024-06-14 15:59:28 +0400

    Added files for returning values from methods

---
+++ ReturnNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿double usd = 23.73;
+int vnd = UsdToVnd(usd);
+
+Console.WriteLine($"${usd} USD = ${vnd} VND");
+
+int UsdToVnd(double usd)
+{
+    int rate = 23500;
+    return (int)(usd * rate);
+}
