079380535c26825922948ef76adb004725a356e8
Aditya Shibu <as2397@hw.ac.uk>
2024-06-14 23:26:41 +0400

    Completed dice challenge

--- DiceMiniGame/Program.cs
+++ DiceMiniGame/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+
+Console.WriteLine("Would you like to play? (Y/N)");
+if (ShouldPlay())
+{
+    PlayGame();
+}
+
+void PlayGame()
+{
+    var play = true;
+
+    while (play)
+    {
+        var target = GetTarget();
+        var roll = RollDice();
+
+        Console.WriteLine($"Roll a number greater than {target} to win!");
+        Console.WriteLine($"You rolled a {roll}");
+        Console.WriteLine(WinOrLose(roll, target));
+        Console.WriteLine("\nPlay again? (Y/N)");
+
+        play = ShouldPlay();
+    }
+}
+
+int GetTarget()
+{
+    return random.Next(1, 7);
+}
+
+int RollDice()
+{
+    return random.Next(1, 7);
+}
+
+bool ShouldPlay()
+{
+    string response = Console.ReadLine();
+    return response.ToLower() == "y";
+}
+
+string WinOrLose(int roll, int target)
+{
+    return roll > target ? "You win!" : "You lose!";
+}
