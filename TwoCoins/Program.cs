acdc94ed22a1652fbe63801c49f41f7644d84067
Aditya Shibu <as2397@hw.ac.uk>
2024-06-14 23:02:31 +0400

    Completed subset problem

--- TwoCoins/Program.cs
+++ TwoCoins/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int target = 10;
+int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+int[] result = TwoCoins(coins, target);
+
+if (result.Length == 0)
+{
+    Console.WriteLine("No two coins make change");
+}
+else
+{
+    Console.WriteLine($"Change found at positions {result[0]} and {result[1]}");
+}
+
+int[] TwoCoins(int[] coins, int target)
+{
+    for (int curr = 0; curr < coins.Length; curr++)
+    {
+        for (int next = curr + 1; next < coins.Length; next++)
+        {
+            if (coins[curr] + coins[next] == target)
+            {
+                return new int[] { curr, next };
+            }
+        }
+    }
+    return new int[0];
+}
