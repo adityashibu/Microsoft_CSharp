3892a05c4ad8402b8254ef437a895c37b43616d8
Aditya Shibu <as2397@hw.ac.uk>
2024-06-14 11:44:02 +0400

    Created First Method for displaying random numbers

--- FirstMethod/Program.cs
+++ FirstMethod/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Generating random numbers: ");
+DisplayRandomNumbers();
+
+void DisplayRandomNumbers()
+{
+    Random random = new Random();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write(random.Next(1, 100) + " ");
+    }
+}
