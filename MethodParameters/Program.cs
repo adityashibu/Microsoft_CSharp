1988525797be48b78b88ee536acbede3ea2ee00b
Aditya Shibu <as2397@hw.ac.uk>
2024-06-14 15:33:17 +0400

    Completed code challenge

--- MethodParameters/Program.cs
+++ MethodParameters/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[,] corporate =
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external =
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+
+for (int i = 0; i < corporate.GetLength(0); i++)
+{
+    // display internal email addresses
+    DisplayEmail(corporate[i, 0], corporate[i, 1], "contoso.com");
+}
+
+for (int i = 0; i < external.GetLength(0); i++)
+{
+    // display external email addresses
+    DisplayEmail(external[i, 0], external[i, 1], externalDomain);
+}
+
+void DisplayEmail(string firstname, string lastname, string domain)
+{
+    string firstpart = firstname.Substring(0, 2).ToLower();
+    string lastpart = lastname.ToLower();
+    Console.WriteLine($"{firstpart}{lastpart}@{domain}");
+}
